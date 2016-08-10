4ed225288815ab8264a1af8895d46c8dbefecd4d
Jonathan Siebert <siebertjonathan@aim.com>
2016-08-10 16:38:41 +0200

    Added tests for addition and removal of queries

--- Tests/test_queryString.py
+++ Tests/test_queryString.py
@@ -69,5 +69,113 @@ class test_addingRemovingCategories(test_queryString):
             self.queryString.removeCategory("cond-mat")
 
 
+class test_addingRemovingSearchQueries(test_queryString):
+    def test_addAuthors(self):
+        self.queryString.addAuthorQuery("Testfrau_T")
+        self.queryString.addAuthorQuery("Mustermann_M")
+        self.assertEqual(self.queryString.getAllAuthorQueries(),
+                         ["Testfrau_T", "Mustermann_M"])
+
+    def test_addAbstracts(self):
+        self.queryString.addAbstractQuery("Awesome")
+        self.queryString.addAbstractQuery("Stuff")
+        self.assertEqual(self.queryString.getAllAbstractQueries(),
+                         ["Awesome", "Stuff"])
+
+    def test_addTitles(self):
+        self.queryString.addTitleQuery("Awesome")
+        self.queryString.addTitleQuery("Stuff")
+        self.assertEqual(self.queryString.getAllTitleQueries(),
+                         ["Awesome", "Stuff"])
+
+    def test_addExistingAuthors(self):
+        self.queryString.addAuthorQuery("Testfrau_T")
+        self.queryString.addAuthorQuery("Testfrau_T")
+        self.assertEqual(self.queryString.getAllAuthorQueries(),
+                         ["Testfrau_T"])
+
+    def test_addExistingAbstracts(self):
+        self.queryString.addAbstractQuery("Awesome")
+        self.queryString.addAbstractQuery("Awesome")
+        self.assertEqual(self.queryString.getAllAbstractQueries(), ["Awesome"])
+
+    def test_addExistingTitles(self):
+        self.queryString.addTitleQuery("Awesome")
+        self.queryString.addTitleQuery("Awesome")
+        self.assertEqual(self.queryString.getAllTitleQueries(), ["Awesome"])
+
+    def test_removeExistingAuthors(self):
+        self.queryString.addAuthorQuery("Testfrau_T")
+        self.queryString.addAuthorQuery("Mustermann_M")
+        self.queryString.removeAuthorQuery("Testfrau_T")
+        self.assertEqual(self.queryString.getAllAuthorQueries(),
+                         ["Mustermann_M"])
+
+    def test_removeExistingAbstracts(self):
+        self.queryString.addAbstractQuery("Awesome")
+        self.queryString.addAbstractQuery("Stuff")
+        self.queryString.removeAbstractQuery("Awesome")
+        self.assertEqual(self.queryString.getAllAbstractQueries(),
+                         ["Stuff"])
+
+    def test_removeExistingTitles(self):
+        self.queryString.addTitleQuery("Awesome")
+        self.queryString.addTitleQuery("Stuff")
+        self.queryString.removeTitleQuery("Awesome")
+        self.assertEqual(self.queryString.getAllTitleQueries(),
+                         ["Stuff"])
+
+    def test_removeNotExistingAuthors(self):
+        self.queryString.addAuthorQuery("Mustermann_M")
+        with self.assertRaises(NotInQueryException):
+            self.queryString.removeAuthorQuery("Testfrau_T")
+
+    def test_removeNotExistingAbstracts(self):
+        self.queryString.addAbstractQuery("Stuff")
+        with self.assertRaises(NotInQueryException):
+            self.queryString.removeAbstractQuery("Awesome")
+
+    def test_removeNotExistingTitles(self):
+        self.queryString.addTitleQuery("Stuff")
+        with self.assertRaises(NotInQueryException):
+            self.queryString.removeTitleQuery("Awesome")
+
+    def test_removeAllExistingAuthors(self):
+        self.queryString.addAuthorQuery("Testfrau_T")
+        self.queryString.addAuthorQuery("Mustermann_M")
+        self.queryString.removeAllAuthorQueries()
+        self.assertEqual(self.queryString.getAllAuthorQueries(),
+                         [])
+
+    def test_removeAllExistingAbstracts(self):
+        self.queryString.addAbstractQuery("Awesome")
+        self.queryString.addAbstractQuery("Stuff")
+        self.queryString.removeAllAbstractQueries()
+        self.assertEqual(self.queryString.getAllAbstractQueries(),
+                         [])
+
+    def test_removeAllExistingTitles(self):
+        self.queryString.addTitleQuery("Awesome")
+        self.queryString.addTitleQuery("Stuff")
+        self.queryString.removeAllTitleQueries()
+        self.assertEqual(self.queryString.getAllTitleQueries(),
+                         [])
+
+    def test_removeEmptyAuthors(self):
+        self.queryString.removeAllAuthorQueries()
+        self.assertEqual(self.queryString.getAllAuthorQueries(),
+                         [])
+
+    def test_removeEmptyAbstracts(self):
+        self.queryString.removeAllAbstractQueries()
+        self.assertEqual(self.queryString.getAllAbstractQueries(),
+                         [])
+
+    def test_removeEmptyTitles(self):
+        self.queryString.removeAllTitleQueries()
+        self.assertEqual(self.queryString.getAllTitleQueries(),
+                         [])
+
+
 if __name__ == "__main__":
     unittest.main()

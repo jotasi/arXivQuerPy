6a61b295a3b7a6ae010113fdbd879e0f7e5022d6
Jonathan Siebert <siebertjonathan@aim.com>
2016-08-11 10:39:43 +0200

    Added functionality for FeedDownloader functions

---
+++ feedDownloader.py
@@ -0,0 +1,85 @@
+import urllib
+import feedparser
+import pickle
+import os
+
+
+class NoDownloadedFeedException(Exception):
+    """Raised when no feed was downloaded, yet.
+    """
+
+
+class NoSavedFeedException(Exception):
+    """Raised when no saved feed is found
+    """
+
+
+class FeedDownloader():
+    """Downloads and parses xml file using arXiv's api for a given query string
+    """
+    def __init__(self, queryString):
+        """Constructor for a FeedDownloader
+
+        Parameters
+        ----------
+        queryString: str
+            String to use for arXiv api query
+        """
+        self.queryString = queryString
+        self.feed = None
+
+    def updateQueryString(self, queryString):
+        """Change the query string
+
+        Parameters
+        ----------
+        queryString: str
+            String to use for arXiv api query
+        """
+        self.queryString = queryString
+
+    def getQueryString(self):
+        """Getter for the query string
+
+        Returns
+        -------
+        queryString: str
+            current query string
+        """
+        return self.queryString
+
+    def updateFeed(self):
+        """Downloads the current feed
+        """
+        self.feed = feedparser.parse(urllib.urlopen(self.queryString).read())
+
+    def getFeed(self):
+        """Returns the current feed
+
+        Raises
+        ------
+        NoDownloadedFeedException
+            If no feed was downloaded, yet. Call updateFeed first!
+        """
+        if self.feed is None:
+            raise NoDownloadedFeedException
+        return self.feed
+
+    def saveFeed(self):
+        """Saves the feed for further use
+        """
+        if self.feed is None:
+            raise NoDownloadedFeedException
+        pickle.dump(self.feed, open("./feed.pickle", "wb"))
+
+    def loadFeed(self):
+        """Loads the feed for further use
+
+        Raises
+        ------
+        NoSavedFeedException
+            If no saved feed is found
+        """
+        if not os.path.exists("./feed.pickle"):
+            raise NoSavedFeedException
+        self.feed = pickle.load(open("./feed.pickle", "rb"))

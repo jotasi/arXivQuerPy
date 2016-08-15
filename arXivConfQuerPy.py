7ef8302e885e70a66f8a5a913f9b44d8ed8a90bd
Benjamin Trefz <xxx>
2016-08-15 14:20:52 +0200

    Stability improvements to the config method.
    Added documentation

--- arXivConfQuerPy.py
+++ arXivConfQuerPy.py
@@ -15,18 +15,38 @@ class FileExistsAlready(Exception):
 
 
 class config():
+    """Class that contains the configuration implementation of the arXiv
+    updater.
+    """
     def __init__(self, filename=".arXivQuerPyconfig"):
+        """Constructor of the config class
+
+        Parameters
+        ----------
+        filename: string
+            path to the config file
+        """
         self.args = {}
         self.filename = filename
 
     def get_args(self):
+        """Returns the found arguments from the config files as a dict
+        """
         return self.args
 
     def config_available(self):
+        """Checks if the given config file is available.
+        """
         return os.path.isfile(self.filename)
 
     def config_read(self):
+        """Reads in the given config file.
+        """
         self.args = {}
+
+        if not self.config_available():
+            raise Exception("File {0} does not exist.".format(self.filename))
+
         with open(self.filename) as config:
             content = config.read()
 
@@ -51,6 +71,10 @@ class config():
                         self.args[key] = split[1].strip()
 
     def generate_config(self):
+        """Generator for the config file.
+        Can only be called if the config file does not exist already.
+        Requires user input, and can therefore not be used in a script.
+        """
         if self.config_available():
             raise FileExistsAlready("File {0} already exists."
                                     .format(self.filename))
@@ -66,7 +90,7 @@ category  = {category}
 title     = {title}
 
 # authors that should be searched (space separated list)
-authors  = {author}
+authors   = {author}
 
 # keywords in abstract that should be searched (space separated list)
 abstract  = {abstract}
@@ -97,31 +121,39 @@ lastNDays = {lastNDays}
 
 
 class DictToAttributes:
+    """A helper class that turns a dict into a class with each key of the dict
+    as an attribute of the class
+    """
     def __init__(self, **entries):
         self.__dict__.update(entries)
 
 
 if (__name__ == "__main__"):
-    conf = config('.arXivQuerPyconfig')
+    # Get the real path to the config file
+    scriptpath = os.path.dirname(os.path.realpath(__file__))
+    configpath = os.path.join(scriptpath, '.arXivQuerPyconfig')
+    conf = config(configpath)
+
+    # Check if a config file exists already, and
+    # if not generate it together with the user.
     if not conf.config_available():
         print("Conf file not found create one first.")
         conf.generate_config()
 
+    # Read in the arguments of the config file
     conf.config_read()
     if conf.get_args() == {}:
         print("No arguments available")
         sys.exit(1)
-    else:
-        args = conf.get_args()
-        print args
-
+    args = conf.get_args()
     args = DictToAttributes(**args)
 
+    # Generate the updater and feed in the arguments.
     if args.lastNDays is None:
         querPy = arXivQuerPy()
     else:
-        querPy = arXivQuerPy(date=datetime.date.today()
-                             - datetime.timedelta(days=int(args.lastNDays)))
+        querPy = arXivQuerPy(date=datetime.date.today() -
+                             datetime.timedelta(days=int(args.lastNDays)))
 
     if not (args.category is None):
         querPy.addCategory(args.category.split(" "))
@@ -134,11 +166,14 @@ if (__name__ == "__main__"):
     if args.suppress.lower() == "true":
         args.suppress = True
 
-    args.suppress = False
-
+    # Do the search
     try:
         querPy.search()
     except qS.EmptyQueryException:
         exit(1)
 
-    querPy.sendMail(args.email, args.suppress)
+    # Send the new papers if applicable
+    if (querPy.sendMail(args.email, args.suppress)):
+        print("Mail send.")
+    else:
+        print("No new papers matching the search.")
